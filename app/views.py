056eba265dada7084d2e12cd95fbd249dea6212e
dev-shah-2204 <devhirenshah2211@gmail.com>
2024-11-07 16:59:40 +0530

    Remove first form

--- app/views.py
+++ app/views.py
@@ -15,41 +15,30 @@ def check_stationarity(res):
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        # date = str(request.form.get('date'))
+    path = './static/images/'
+    dag = bayesnet.get_dag()
+    optimised_dag = bayesnet.dag_conditional_correlation()
 
+    date_range = '2011 to 2024'
+    adf_result = bayesnet.get_adf_result()
+    is_stationary = check_stationarity(adf_result)
+    step = request.form.get('step')
 
-        # if date == "2011":
-        #     from . import bayesnet22 as bayesnet
-        #     date_range = "2011 to 2022"
-        #
-        # elif date == "2022":
-        #     from . import bayesnet24 as bayesnet
-        #     date_range = "2022 to 2024"
-
-        date_range = "2011 to 2022"
-        adf_result = bayesnet.get_adf_result()
-        is_stationary = check_stationarity(adf_result)
-
-        step = request.form.get('step')
-
+    if request.method == 'GET':
         if not is_stationary:
-            adf_result_table = adf_result.to_html(classes="table table-striped", index=False)
+            adf_result_table = adf_result.to_html(classes='table table-striped', index=False)
             correlation_matrix_table = None
 
-        if step == "next":
-            bayesnet.get_dag()
-
+    elif request.method == 'POST':
+        if step == 'next':
             adf_result = bayesnet.get_adf_after_diff()
             is_stationary = check_stationarity(adf_result)
-            adf_result_table = adf_result.to_html(classes="table table-striped", index=False)
+            adf_result_table = adf_result.to_html(classes='table table-striped', index=False)
 
             correlation_matrix = bayesnet.get_correlation_matrix()
-            correlation_matrix_table = correlation_matrix.to_html(classes="table table-striped", index=False)
+            correlation_matrix_table = correlation_matrix.to_html(classes='table table-striped', index=False)
 
-        return render_template('index.html', is_stationary=is_stationary, show_res=True, adf_table=adf_result_table, correlation_matrix_table=correlation_matrix_table, date_range=date_range)
-
-    return render_template('index.html', show_res=False)
+    return render_template('index.html', is_stationary=is_stationary, show_res=True, adf_table=adf_result_table, correlation_matrix_table=correlation_matrix_table, date_range=date_range, dag=f"{path}{dag}", optimised_dag=f"{path}{optimised_dag}")
 
 
 @views.route('/test', methods=['GET'])
